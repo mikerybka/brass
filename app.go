56fb7a3e718126691c332d5536ea154cc3aa4a8a
Mike Rybka <merybka@gmail.com>
2025-02-24 16:50:36 -0500

    wip

--- app.go
+++ app.go
@@ -18,36 +18,56 @@ func (a *App) genMetaPkg(path string) error {
 	return nil
 }
 
-func (a *App) Build(tag string) error {
+func (a *App) genAPIAuthPkg(path string) error {
+	return nil
+}
+
+func (a *App) genAPIDataPkg(path string) error {
+	return nil
+}
+
+func (a *App) genAPIPkg(path string) error {
+	return nil
+}
+
+func (a *App) genDockerfile(path string) error {
+	return nil
+}
+
+func (a *App) genMainGo(path string) error {
+	return nil
+}
+
+func (a *App) Generate(dir string) error {
 	// Create work dir
-	workdir, err := os.MkdirTemp(os.TempDir(), "")
+	err := os.MkdirAll(dir, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
 	// Generate pkg/meta
-	path := filepath.Join(workdir, "pkg/meta")
+	path := filepath.Join(dir, "pkg/meta")
 	err = a.genMetaPkg(path)
 	if err != nil {
 		return fmt.Errorf("generating pkg/meta: %s", err)
 	}
 
 	// Generate pkg/api/auth
-	path = filepath.Join(workdir, "pkg/api/auth")
+	path = filepath.Join(dir, "pkg/api/auth")
 	err = a.genAPIAuthPkg(path)
 	if err != nil {
 		return fmt.Errorf("generating pkg/api/auth: %s", err)
 	}
 
 	// Generate pkg/api/data
-	path = filepath.Join(workdir, "pkg/api/data")
+	path = filepath.Join(dir, "pkg/api/data")
 	err = a.genAPIDataPkg(path)
 	if err != nil {
 		return fmt.Errorf("generating pkg/api/data: %s", err)
 	}
 
 	// Generate pkg/api
-	path = filepath.Join(workdir, "pkg/api")
+	path = filepath.Join(dir, "pkg/api")
 	err = a.genAPIPkg(path)
 	if err != nil {
 		return fmt.Errorf("generating pkg/api: %s", err)
@@ -60,9 +80,18 @@ func (a *App) Build(tag string) error {
 	// TODO
 
 	// Generate main.go
+	path = filepath.Join(dir, "main.go")
+	err = a.genMainGo(path)
+	if err != nil {
+		return fmt.Errorf("generating pkg/api: %s", err)
+	}
 
 	// Generate Dockerfile
-	// Run docker build
+	path = filepath.Join(dir, "Dockerfile")
+	err = a.genDockerfile(path)
+	if err != nil {
+		return fmt.Errorf("generating pkg/api: %s", err)
+	}
 
-	// Delete workdir
+	return nil
 }

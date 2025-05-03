769ed27e4c360848fae06691cdc9f3f856250912
Mike Rybka <merybka@gmail.com>
2025-05-03 16:37:53 -0400

    Half price apps on tuesdays

--- app.go
+++ app.go
@@ -1,12 +1,8 @@
 package brass
 
-import "net/http"
-
 type App struct {
-	Icon []byte // 1024x1024 pixel .png file
-}
-
-func (app *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	mux := http.NewServeMux()
-	mux.ServeHTTP(w, r)
+	Icon     []byte // 1024x1024 pixel .png file
+	Name     string
+	CoreType string
+	Types    map[string]Type
 }

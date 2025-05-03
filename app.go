24358df1e6c6613ca034369526c2002759d3d989
Mike Rybka <merybka@gmail.com>
2025-05-02 21:35:55 -0400

    rewording

--- app.go
+++ app.go
@@ -3,7 +3,7 @@ package brass
 import "net/http"
 
 type App struct {
-	Icon []byte // 1024px x 1024px .png file
+	Icon []byte // 1024x1024 pixel .png file
 }
 
 func (app *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {

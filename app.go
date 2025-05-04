b32b3cc85d89d39e277503204a138b6436bd8212
Mike Rybka <merybka@gmail.com>
2025-05-03 21:49:26 -0400

    Stub App.GenerateSourceCode()

--- app.go
+++ app.go
@@ -1,9 +1,25 @@
 package brass
 
+import (
+	"fmt"
+
+	"github.com/mikerybka/util"
+)
+
 type App struct {
 	Repo     string
 	Name     string
 	Icon     []byte // 1024x1024 pixel .png file
 	CoreType string
 	Types    map[string]Type
+}
+
+func (a *App) GenerateSourceCode(dir string) error {
+	errs := util.RunInParallel([]func() error{})
+	for i, err := range errs {
+		if err != nil {
+			return fmt.Errorf("%d: %s", i, err)
+		}
+	}
+	return nil
 }
